616096b1bbe8c59311abe9eac124c4dd8c4b76e9
kghandhi <kghandhi@uchicago.edu>
2014-05-11 14:23:02 -0500

    some stuff done maybe?

--- hw2/paxos.py
+++ hw2/paxos.py
@@ -1,12 +1,12 @@
 proposal_id = 0
 
 class Message(object):
-    def __init__(self, value, typ, src, dst, ID):
+    def __init__(self, value, typ, src, dst, n):
         self.value = value #thing
         self.typ = typ #string PROPOSE, PREPARE, PROMISE, ACCEPT, ACCEPTED, REJECTED
         self.src = src #list, make sure this isnt a list and can reference an actual computer
         self.dst = dst #possibly will be like p_1 should change somehow
-        self.ID = ID #if not a proposal should this ID be negative?
+        self.n = n #if not a proposal should this ID be negative?
 
 def Queue_Message(N, m):
     N.append(m)
@@ -24,4 +24,21 @@ def Extract_Message(N):
 
 #should be in the definition of the acceptor or the proposer, 
 #but maybe not because it takes a computer            
-def Deliver_Message(src_p, msg):
+class Proposer(object):
+    def __init__(self, ID, accs,...):
+        self.ID = ID
+        self.failed = False
+        self.accs = accs #list of acceptors
+    def Deliver_Message(self, N, c, msg):
+        if msg.typ == "PROPOSE":
+            proposal_id += 1
+            for c_a in self.accs:
+                Queue_Message(N, Message(msg.pi_v, "PREPARE", self.ID, c_a.ID, proposal_id))
+        if msg.typ == 
+
+class Acceptor(object):
+    def __init__(self, ID, ...):
+        self.ID
+        self.failed = False
+
+
--- hw2/simulate.py
+++ hw2/simulate.py
@@ -1,54 +1,104 @@
-class Proposer(object):
-    def __init__(self, ID, ...):
-        self.ID = ID
-        self.failed = False
 
-class Acceptor(object):
-    def __init__(self, ID, ...):
-        self.ID
-        self.failed = False
+class Event(object):
+    def __init__(self, t, F, R, pi_c, pi_v):
+        self.t = t
+        self.F = F #dictionary of computers who fail       
+        self.R = R #dictionary of computers who recover         
+        self.pi_c = pi_c #proposer ID proposing something    these are lists
+        self.pi_v = pi_v #value proposer proposes. both this and pi_v should be null if either is
 
-class Message(object):
-    def __init__(self, value, typ, src, dst):
-        self.value = value #thing
-        self.typ = typ #string
-        self.src = src #list
-        self.dst = dst #possibly will be like p_1 should change somehow
-
-def simulate(n_p, n_a, t, E):
-    props = []
-    accs = []
-    for i in xrange(n_p):
-        props.append(Proposer(i+1, ...))
-
+def simulate(n_p, n_a, t_max, E):
+    props = [] #static list of proposers (access proposer i by doing props[i-1]
+    accs = [] #static list of acceptors (access acceptor i by doing accs[i-1]
+    
     for j in xrange(n_a):
         accs.append(Acceptor(j+1, ...))
+    for i in xrange(n_p):
+        props.append(Proposer(i+1, accs))
 
-    N = [] #empty network? should it be a se?
+    N = [] #empty network? should it be a set? this is a queue of messages, will change
 
-    for i in xrange(t):
+    for i in xrange(t_max):
         if (len(N) == 0) and (len(E) == 0):
             return
         for event in E:
-            if event[0] = t:
+            if event.t == i:
                 e = event
         if len(e) != 0:
             E.remove(e)
-            for c in e[1]: #F failed computers
-                c.failed = True #figure this out, should p_1 or a_1 refer to the object?
-            for c in e[2]: #R revived computers
-                c.failed = False
-            if (len(e[3]) != 0) and (len(e[4]) != 0): #pi_c = proposer, pi_v = value proposed
-                m = Message(e[4][0], "PROPOSE", [], e[3][0])
-                Deliver_Message(e[3][0], m)
+            # for c in e.F: #F failed computers
+            #     c.failed = True #figure this out, should p_1 or a_1 refer to the object?
+            #  for c in e.R: #R revived computers
+            #     c.failed = False
+            for p_id in e.F['P']:
+                props[p_id - 1].failed = True
+            for a_id in e.F['A']:
+                accs[a_id - 1].failed = True
+            for p_id in e.R['P']:
+                props[p_id - 1].failed = False
+            for a_id in e.R['A']:
+                accs[a_id - 1].failed = False 
+           
+            if (len(e.pi_c) != 0) and (len(e.pi_v) != 0): #pi_c = proposer, pi_v = value proposed
+                msg = Message(e.pi_v[0], "PROPOSE", [], e.pi_c[0])
+                Deliver_Message(e.pi_c[0], msg)
             else:
-                m = Extract_Message(N)
-                if len(m) != 0: #resolve!
-                    Deliver_Message(m.dst, m)
+                msg = Extract_Message(N)
+                if msg != 0: #resolve!
+                    Deliver_Message(msg.dst, msg)
         else:
-            m = Extract_Message(N):
-            if len(m) != 0: #resolve!
-                Deliver_Message(m.dst,m)
+            msg = Extract_Message(N):
+            if msg != 0: #resolve!
+                Deliver_Message(msg.dst, msg)
                
              
+def main(n_p, n_a, t_max, E):
+    simulate(n_p, n_a, t_max, E)
+    
+if __name__ == '__main__':
+    E = []
+    t_max = int(t_max_s)
+    ev_dic = {}
+    #i think id need to reverse these loops
+        
+        for l in fileinput.input():
+            line = l.strip('\n').split(" ")
+            if (len(line) == 2) and (line[1] == "END"): 
+                #order ev_dic by key then put in E
+                break
+            if (len(line) == 3):
+                n_p, n_a, t_max_s = line
+            elif (len(line) == 4):
+                if line[0] in ev_dic:
+                    e = ev_dic[line[0]] 
+                    if line[1] == "FAIL":
+                        e.F[line[2][0]].append(line[3])
+                    elif line[1] == "RECOVER":
+                        e.R[line[2][0]].append(line[3])
+                    elif line[1] == "PROPOSE":
+                        e.pi_c.append(line[2])
+                        e.pi_v.append(line[3])
+                    else: 
+                        print "INVALID INPUT"
+                else:
+                    F = {'P': [], 'A': []}
+                    R = {'P': [], 'A': []}
+                    pi_c = []
+                    pi_v = []
+                    if line[1] == "FAIL":
+                        F[line[2][0]].append(line[3])
+                    elif line[1] == "RECOVER":
+                        R[line[2][0]].append(line[3])
+                    elif line[1] == "PROPOSE":
+                        pi_c.append(line[2])
+                        pi_v.append(line[3])
+                    else: 
+                        print "INVALID INPUT"
+                    ev_dic[line[0]] = Event(line[0], F, R, pi_c, pi_v)
+                    
+    main(int(n_p), int(n_a), t_max, E)
                 
+           
+                
+
+           
